252f564fd86f74be91a184cee577c85463b91770
AmickL <amickl29@yahoo.com>
2026-01-21 14:10:19 -0800

    Update signaling server to new Cloudflare tunnel URL

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     // Signaling server HTTP API URL
     // Production: https://your-signaling-server.com:8001
     // Development: http://localhost:8001
-    SIGNALING_SERVER: 'https://dallas-found-sale-hash.trycloudflare.com',
+    SIGNALING_SERVER: 'https://packages-improvements-pharmacies-affair.trycloudflare.com',
 
     // OAuth callback URL (this dashboard's URL + /callback.html)
     // For GitHub Pages: https://yourusername.github.io/sleap-RTC/dashboard/callback.html
